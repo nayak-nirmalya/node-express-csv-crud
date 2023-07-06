663fc3475b49ce0daf4e6c2130b89ee53520c84a
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-06 19:26:22 +0530

    added test cases for GET /api/v1/users/

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -2,14 +2,58 @@ import request from "supertest";
 
 const url = "http://localhost:3000";
 
-describe("Get Users", () => {
-  // test("should be listening", async () => {
-  //   const res = await request("http://localhost:3000").get("/api/v1/users/");
-  //   console.log(res.body);
-  //   expect(res.body).toEqual({ message: "All Good to Go!" });
-  // });
-
+describe("GET /api/v1/users/", () => {
   it("should be listening", async () => {
     await request(url).get("/api/v1/users/").expect(200);
+  });
+
+  it("should return users list with length greator than 0", async () => {
+    const res = await request(url).get("/api/v1/users/");
+    expect(res.body.users.length).toBeGreaterThan(0);
+  });
+
+  it("should have properties", async () => {
+    const res = await request(url).get("/api/v1/users/");
+
+    expect(res.body).toHaveProperty("users");
+
+    expect(res.body.users[1]).toHaveProperty("id");
+    expect(res.body.users[1]).toHaveProperty("first_name");
+    expect(res.body.users[1]).toHaveProperty("last_name");
+    expect(res.body.users[1]).toHaveProperty("email");
+    expect(res.body.users[1]).toHaveProperty("mob_no");
+    expect(res.body.users[1]).toHaveProperty("address");
+  });
+
+  it("responds with json", async () => {
+    await request(url).get("/api/v1/users/").expect("Content-Type", /json/);
+  });
+});
+
+describe("GET /api/v1/users/:id", () => {
+  it("should be listening", async () => {
+    await request(url).get("/api/v1/users/").expect(200);
+  });
+
+  it("should return users list with length greator than 0", async () => {
+    const res = await request(url).get("/api/v1/users/");
+    expect(res.body.users.length).toBeGreaterThan(0);
+  });
+
+  it("should have properties", async () => {
+    const res = await request(url).get("/api/v1/users/");
+
+    expect(res.body).toHaveProperty("users");
+
+    expect(res.body.users[1]).toHaveProperty("id");
+    expect(res.body.users[1]).toHaveProperty("first_name");
+    expect(res.body.users[1]).toHaveProperty("last_name");
+    expect(res.body.users[1]).toHaveProperty("email");
+    expect(res.body.users[1]).toHaveProperty("mob_no");
+    expect(res.body.users[1]).toHaveProperty("address");
+  });
+
+  it("responds with json", async () => {
+    await request(url).get("/api/v1/users/").expect("Content-Type", /json/);
   });
 });
