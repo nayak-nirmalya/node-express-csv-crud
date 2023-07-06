9fe5bdd2497f1962fd947e3b3413bfa23352b1a0
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-06 14:08:43 +0530

    added validation middleware

---
+++ src/backend/middleware/validate.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { Request, Response, NextFunction } from "express";
+
+const User = z.object({
+  first_name: z
+    .string({
+      required_error: "First Name is Required."
+    })
+    .nonempty(),
+  last_name: z
+    .string({
+      required_error: "Last Name is Required."
+    })
+    .nonempty(),
+  email: z
+    .string({
+      required_error: "E-Mail is Required."
+    })
+    .email("Not a Valid E-Mail.")
+    .nonempty(),
+  mob_no: z
+    .string({
+      required_error: "Mobile Number is Required."
+    })
+    .regex(new RegExp(/^\d{10}$/)),
+  address: z
+    .string({
+      required_error: "Address is Required."
+    })
+    .nonempty()
+    .min(4)
+});
+
+export const validate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    await User.parseAsync(req.body);
+    next();
+  } catch (error) {
+    return res.status(409).json({
+      status: "Failed",
+      error
+    });
+  }
+};
--- src/backend/routes/main.ts
+++ src/backend/routes/main.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
 import { createUser } from "../controllers/main.js";
+import { validate } from "../middleware/validate.js";
 
 const router = Router();
 
-router.route("/").post(createUser);
+router.route("/").post(validate, createUser);
 
 export default router;
