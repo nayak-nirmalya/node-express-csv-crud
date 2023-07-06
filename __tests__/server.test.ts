d5ee2239bb0492095eacd9b291fa526968dd3f2d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-06 19:01:23 +0530

    added first test

--- __tests__/server.test.ts
+++ __tests__/server.test.ts
@@ -1,5 +1,15 @@
-describe("Server.ts tests", () => {
-  test("Math test", () => {
-    expect(2 + 2).toBe(4);
+import request from "supertest";
+
+const url = "http://localhost:3000";
+
+describe("Get Users", () => {
+  // test("should be listening", async () => {
+  //   const res = await request("http://localhost:3000").get("/api/v1/users/");
+  //   console.log(res.body);
+  //   expect(res.body).toEqual({ message: "All Good to Go!" });
+  // });
+
+  it("should be listening", async () => {
+    await request(url).get("/api/v1/users/").expect(200);
   });
 });
